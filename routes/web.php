d43c27d26fb5167f46f9ef7230bc2b6d243c1fe5
fathanfadillah <fathan17698@gmail.com>
2022-05-13 08:09:13 +0700

    add layouting

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,8 @@ use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name("/");
 
-Route::get('product.index', [ProductController::class, "index"])->name('product.index');;
-Route::get('product.create', [ProductController::class, "create"])->name('product.create');
-Route::post('product.store', [ProductController::class, "store"])->name('product.store');
+Route::get('product/index', [ProductController::class, "index"])->name('product/index');;
+Route::get('product/create', [ProductController::class, "create"])->name('product/create');
+Route::post('product/store', [ProductController::class, "store"])->name('product/store');
